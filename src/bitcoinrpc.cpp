58f660669d3165bf149da53011bac07221fbf32e
Anch <anch@subba.net>
2014-03-05 11:38:11 -0800

    Network Changes and Tested Make for osx and Linux

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -39,7 +39,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 31337 : 1337;
+    return GetBoolArg("-testnet", false) ? 39337 : 9337;
 }
 
 Object JSONRPCError(int code, const string& message)
